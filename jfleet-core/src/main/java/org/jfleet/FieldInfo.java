349c933c427ab271ec2cff9665ac906772ccaee1
Jeronimo Lopez <jerolba@MacBook-Pro-de-Jeronimo.local>
2018-01-31 20:53:25 +0100

    Remove unused code

--- jfleet-core/src/main/java/org/jfleet/FieldInfo.java
+++ jfleet-core/src/main/java/org/jfleet/FieldInfo.java
@@ -54,36 +54,6 @@ public class FieldInfo {
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((columnName == null) ? 0 : columnName.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        FieldInfo other = (FieldInfo) obj;
-        if (columnName == null) {
-            if (other.columnName != null) {
-                return false;
-            }
-        } else if (!columnName.equals(other.columnName)) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
     public String toString() {
         return fieldName + " (" + columnName + "): " + fieldType.getFieldType().name();
     }
