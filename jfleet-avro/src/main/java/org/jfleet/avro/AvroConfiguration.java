dc9cdaf4c17c86f9040f410cb04a2c8165fbef21
Álvaro Moraleda <alvaro.moraleda@clarity.ai>
2022-07-15 18:02:49 +0200

    Avro configuration refactor

--- jfleet-avro/src/main/java/org/jfleet/avro/AvroConfiguration.java
+++ jfleet-avro/src/main/java/org/jfleet/avro/AvroConfiguration.java
@@ -2,14 +2,26 @@ package org.jfleet.avro;
 
 import org.jfleet.EntityInfo;
 
-public class AvroConfiguration {
-    private final EntityInfo entityInfo;
+public class AvroConfiguration<T> {
+
+    private EntityInfo entityInfo;
+    private Class<T> clazz;
 
     public AvroConfiguration(EntityInfo entityInfo) {
         this.entityInfo = entityInfo;
+        clazz = (Class<T>) entityInfo.getEntityClass();
+    }
+
+    @SuppressWarnings("unchecked")
+    public AvroConfiguration(Class<T> clazz) {
+        this.clazz = clazz;
     }
 
     public EntityInfo getEntityInfo() {
         return entityInfo;
+    }
+
+    public Class<T> getClazz() {
+        return clazz;
     }
 }
